93cc8e264bc3e31a309a2dd68277c725fee7de45
toewailin <toewailinbdl@gmail.com>
2025-02-06 14:54:41 +0630

    add components and mobile header and footer nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,12 @@
     <h1 class="text-3xl font-bold underline">
         Hello, World!
     </h1>
+    <x-accommodation-section></x-accommodation-section>
+    <x-oversea-section></x-oversea-section>
+    <x-popular-section></x-popular-section>
+    <x-vacation-section></x-vacation-section>
+    <x-city-center-section></x-city-center-section>
+    <x-today-hot-section></x-today-hot-section>
     <x-special-offer-section></x-special-offer-section>
     <x-exhibition-section></x-exhibition-section>
 </x-guest-layout>
